9a0173109570b91f7c119e20ee132303b2720112
rhuan <rhuan.resende@hotmail.com.br>
2026-09-23 22:13:48 -0300

    feat: First commit

---
+++ src/main/java/com/desenvolvimento/logica/cashpilot_api/CashpilotApiApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.logica.cashpilot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashpilotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashpilotApiApplication.class, args);
+	}
+
+}
